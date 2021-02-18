e1136bf062f7476bae545a95e1bffd052be7f44a
idkosilov <idkosilov@gmail.com>
2021-02-19 04:51:26 +0700

    edit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sympy import *
+from matplotlib import pyplot as plt
 import numpy as np
 
 
@@ -34,35 +35,75 @@ class Problem:
                 else {self.x: self.income / self.Px, self.y: 0}
         return optimum
 
-    def income_consumption_curve(self, start_income, final_income):
-        temp_income = self.income
-        xys = []
-        for income in np.arange(start_income, final_income, (final_income - start_income) / 100):
-            self.income = income
-            xys.append(self.optimum_point())
-        self.income = temp_income
-        return [xy[self.x] for xy in xys], [xy[self.y] for xy in xys]
-
-    def engel_curve(self):
-        self.income = symbols("I")
-        budget = self.Px * self.x + self.Py * self.y - self.income
+    def engel_curves(self):
+        budget = self.Px * self.x + self.Py * self.y - symbols("I")
         x, y = solve(self.mrs_xy - self.Px / self.Py, self.x)[0], solve(self.mrs_xy - self.Px / self.Py, self.y)[0]
-        return solve(budget.subs(self.y, y), self.x)[0], solve(budget.subs(self.x, x), self.y)[0]
+        return solve(budget.subs(self.y, y), dict=True)[0], solve(budget.subs(self.x, x), dict=True)[0]
 
-    def price_consumption_curve(self, start_px, final_px):
-        xys = []
-        for price in np.arange(start_px, final_px, (final_px - start_px) / 100):
-            self.Px = price
-            xys.append(self.optimum_point())
-        return [xy[self.x] for xy in xys], [xy[self.y] for xy in xys]
+    def demand_curves(self):
+        budget = symbols("Px") * self.x + self.Py * self.y - self.income
+        demand_x = solve([self.mrs_xy - symbols("Px") / self.Py, budget], dict=True)[0]
+        budget = self.Px * self.x + symbols("Py") * self.y - self.income
+        demand_y = solve([self.mrs_xy - self.Px / symbols("Py"), budget], dict=True)[0]
+        return demand_x, demand_y
 
-    def demand_curve(self, start_px, final_px):
-        pass
+    def income_consumption_curve(self):
+        engel_x, engel_y = self.engel_curves()
+        return solve(engel_x[symbols("I")] - engel_y[symbols("I")], self.y)[0]
+
+    def show_plots(self):
+        budget = lambdify(self.x, solve(self.Px * self.x + self.Py * self.y - self.income, self.y)[0], "numpy")
+        xy_optimum = self.optimum_point()
+        expr_u = solve(parse_expr(self.utility).subs(list(xy_optimum.items())) - parse_expr(self.utility), self.y)[-1]
+        utility = lambdify(self.x, expr_u, "numpy")
+        x = np.linspace(0.1, self.income / self.Px, 1000)
+        y = np.linspace(0.1, self.income / self.Py, 1000)
+
+        fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(5, 20))
+        axs[0].axis(ymin=0, ymax=self.income / self.Py, xmin=0, xmax=self.income / self.Px)
+        axs[0].set_title("Consumer optimum")
+        axs[0].plot(x, utility(x))
+        axs[0].plot(x, budget(x))
+        axs[0].scatter(xy_optimum[self.x], xy_optimum[self.y], color='red')
+        axs[0].set_xlabel("Product X")
+        axs[0].set_ylabel("Product Y")
+
+        income = np.linspace(0, self.income * 2, 1000)
+        engel = self.engel_curves()
+        income_x = lambdify(self.x, engel[0][symbols("I")], "numpy")
+        income_y = lambdify(self.y, engel[1][symbols("I")], "numpy")
+        axs[1].axis(ymin=0, xmin=0, xmax=max(income_x(income)), ymax=max(income))
+        axs[1].set_title("Engel curve for product X")
+        axs[1].plot(income_x(income), income, color="green")
+        axs[1].set_xlabel("Income I")
+        axs[1].set_ylabel("Product X")
+        axs[2].axis(ymin=0, xmin=0, xmax=max(income_y(income)), ymax=max(income))
+        axs[2].set_title("Engel curve for product Y")
+        axs[2].plot(income_y(income), income, color="gold")
+        axs[2].set_xlabel("Income I")
+        axs[2].set_ylabel("Product Y")
+
+        demand_xy = self.demand_curves()
+        demand_x, demand_y = lambdify(self.x, demand_xy[0][symbols("Px")], "numpy"), lambdify(self.y, demand_xy[1][
+            symbols("Py")], "numpy")
+        axs[3].axis(ymin=0, xmin=0, ymax=max(demand_x(x)), xmax=max(x))
+        axs[3].set_title("Product demand  X")
+        axs[3].plot(x, demand_x(x), color="purple")
+        axs[3].set_xlabel("Product X")
+        axs[3].set_ylabel("Price X")
+        axs[4].axis(ymin=0, xmin=0, ymax=max(demand_y(y)), xmax=max(y))
+        axs[4].set_title("Product demand  Y")
+        axs[4].plot(y, demand_y(y), color="red")
+        axs[4].set_xlabel("Product Y")
+        axs[4].set_ylabel("Price Y")
+
+        fig.tight_layout()
+        plt.show()
 
 
 def main():
-    problem = Problem(utility="x*y", Px=2, Py=1, income=10)
-    print(problem.price_consumption_curve(1, 10))
+    problem = Problem(utility="x*y**4", Px=1, Py=4, income=40)
+    problem.show_plots()
 
 
 if __name__ == "__main__":
